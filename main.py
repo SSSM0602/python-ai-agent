fd7054b7aaec26ea4c02d60b274026ee9bee9e8d
Saketh <saketh.medicherla@gmail.com>
2025-07-20 00:12:17 -0400

    Agent runs defined functions

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.run_python import run_python_file
+from functions.write_file import write_file
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.get_files_info import schema_get_files_info
 from functions.run_python import schema_run_python
@@ -31,6 +35,45 @@ available_functions = types.Tool(
         schema_get_files_info, schema_write_file, schema_run_python, schema_get_file_content
     ]
 )
+working_directory = "./calculator"
+func_defs = {
+    "run_python_file": run_python_file,
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "write_file": write_file
+}
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    try:
+        curr_func = func_defs[function_call_part.name]
+    except:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    
+    func_args = function_call_part.args
+    func_args['working_directory'] = working_directory
+    print(func_args)
+    result = curr_func(**func_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": result},
+            )
+        ],
+    )
+    
 def main():
     if len(sys.argv) < 2:
         print("Need a cmd line argument to use the model!")
@@ -39,12 +82,17 @@ def main():
     user_prompt = sys.argv[1]
     messages = types.Content(role="user", parts=[types.Part(text=user_prompt)])
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, 
-                                              config=types.GenerateContentConfig(
-                                              tools=[available_functions], system_instruction=system_prompt
-                                              )
-                                              ,)
+        config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if len(response.function_calls):
-        print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
+        try:
+            func_call_result = call_function(function_call_part=response.function_calls[0], verbose=(sys.argv[2] == "--verbose"))
+            print(f"{func_call_result.parts[0].function_response.response}")
+        except Exception as e:
+            print(f"Error: {e}")
+        #print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
     else:
         print(response.text)
     if (len(sys.argv) > 2):
